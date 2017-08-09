f3df1f418beb23aa75555fd180c021b76261fc5f
Samuel Novaes <samuel.novaes.96@gmail.com>
2017-08-08 21:10:43 -0300

    08/08/2017 21:10

---
+++ js/calc.js
@@ -0,0 +1,69 @@
+let memory = [];
+let id = 0;
+let index = 0;
+let plot = 0;
+let parser = math.parser();
+
+math.import({
+	plot: (...funcs) => {
+		let data = [];
+		funcs.forEach(fn => {
+			data.push({ fn: fn })
+		});
+		plot++;
+		$('#runner').append($(`<div class="plot" id="p${plot}"></div>`));
+		functionPlot({
+			target: `#p${plot}`,
+			width: 580,
+			height: 400,
+			title: '',
+			grid: true,
+			data
+		});
+	}
+});
+
+$('#f0').focus();
+
+$('#runner').on('keydown', '.field', function (e) {
+	switch (e.key) {
+		case 'Enter':
+			let str = $(this).val();
+			memory.push(str);
+			index = memory.length;
+			if (str == 'clear') {
+				$('#runner').empty();
+			}
+			else {
+				id++;
+				$(this).prop('disabled', true);
+				try {
+					let result = parser.eval(str);
+					if (typeof result == 'function') {
+						result = result.syntax;
+					}
+					$('#runner').append($('<div class="result"></div>').text(result));
+				}
+				catch (err) {
+					$('#runner').append($('<div class="error"></div>').text(err));
+				}
+			}
+			$('#runner').append($(`<input type="text" id="f${id}" class="field" placeholder="Math.js Expression">`));
+			$(`#f${id}`).focus();
+			break;
+		case 'ArrowUp':
+			if (index > 0) {
+				$(this).val(memory[--index]);
+			}
+			break;
+		case 'ArrowDown':
+			if (index < memory.length) {
+				$(this).val(memory[++index]);
+			}
+			break;
+	}
+});
+
+$("#help-link").click(function(){
+	$("#help").toggle();
+});
